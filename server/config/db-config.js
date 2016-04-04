c65e6374b679c4c39cf8144046b6328be3ccb827
Alex Nitta <a.j.nitta@gmail.com>
2016-04-04 16:47:40 -0700

    Add underscore, edits to db and server config

--- server/config/db-config.js
+++ server/config/db-config.js
@@ -7,7 +7,7 @@ var db = mongoose.connection;
 
 db.on('error', console.error.bind(console, 'Database connection error: '));
 db.once('open', function() {
-  console.log('Success! Connected to Mongoose DB')
+  console.log('Success! Connected to Mongoose DB');
 });
 
 module.exports = db;
--- server/config/server-config.js
+++ server/config/server-config.js
@@ -4,17 +4,18 @@ var utility = require('../lib/utility');
 var handler = require('../lib/request-handler');
 var bodyParser = require('body-parser');
 var cookieParser = require('cookie-parser');
+var session = require('express-session');
 
 var app = express();
 
 
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
+// app.set('views', __dirname + '/views');
+// app.set('view engine', 'ejs');
 app.use(partials());
-// app.use(express.bodyParser());
-app.use(express.static(__dirname + '/'));
-app.use(express.cookieParser('shhhh, very secret'));
-app.use(express.session());
+app.use(bodyParser());
+app.use(express.static(__dirname + '../client/index.html'));
+app.use(cookieParser('shhhh, very secret'));
+app.use(session());
 
 app.get('/', handler.renderIndex);
 
