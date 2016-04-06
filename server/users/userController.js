f938482e988ef93fd67006f8d9adbc194ecb48f9
Alex Nitta <a.j.nitta@gmail.com>
2016-04-05 17:38:41 -0700

    Add API endpoint for /users

--- server/users/userController.js
+++ server/users/userController.js
@@ -3,8 +3,6 @@ var crypto = require('crypto');
 var bcrypt = require('bcrypt');
 var util = require('../lib/utility');
 var db = require('../config/db-config');
-
-
 var User = require('./userModel');
 
 exports.renderIndex = function(req, res) {
@@ -12,14 +10,65 @@ exports.renderIndex = function(req, res) {
 };
 
 exports.findAll = function(req, res) {
-  // res.send([{
-  //   username: 'default',
-  //   password: 'default'
-  // }]);
   User.find({}, function(err, results) {
     if (err) {
       console.log('error in userController: ', err);
     }
     return res.send(results);
+  });
+};
+
+exports.import = function(req, res) {
+  User.create(
+    { username: 'bob',
+      password: 'dylan' },
+    { username: 'hank',
+      password: 'williams' },
+    { username: 'john',
+      password: 'prine' },
+    { username: 'townes',
+      password: 'van zandt'}  
+    , function(err) {
+      if (err) {
+        return console.log(err);
+      }
+      return res.send(202);
+    }
+  );
+};
+
+exports.findById = function(req, res) {
+  var id = req.params.id;
+  User.findOne({'_id': id}, function(err, result) {
+    return res.send(result);
+  });
+};
+
+exports.update = function(req, res) {
+  var id = req.params.id;
+  var updates = req.body;
+
+  User.update({'_id': id}, req.body, function (err, numberAffected) {
+    if (err) {
+      return console.log(err);
+    }  
+    console.log('Updated %d users', numberAffected);
+    return res.send(202);
+  });
+};
+
+exports.add = function(req, res) {
+  User.create(req.body, function (err, user) {
+    if (err) {
+      return console.log(err);
+    }
+    return res.send(user);
+  });
+};
+
+exports.delete = function(req, res){
+  var id = req.params.id;
+  User.remove({'_id': id}, function(result) {
+    return res.send(result);
   });
 };
