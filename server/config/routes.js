fb06457a0cc6bb4ec80099331a11da433f3a6ff3
Alex Nitta <a.j.nitta@gmail.com>
2016-04-05 17:58:24 -0700

    Revert "Connect Angular JWT auth to API endpoints"
    This reverts commit df033ee2d3851c6a802cab6b8c86830a64c7e097.

--- server/config/routes.js
+++ server/config/routes.js
@@ -3,4 +3,8 @@ var userController = require('../users/userController.js');
 module.exports = function (app, express) {
   app.get('/', userController.renderIndex);
   app.get('/users', userController.findAll);
+  app.get('/users/import', userController.import);
+  app.get('/users/:id', userController.findById);
+  app.put('/users/:id', userController.update);
+  app.delete('/users/:id', userController.delete);
 };
--- server/users/userController.js
+++ server/users/userController.js
@@ -24,4 +24,58 @@ exports.findAll = function(req, res) {
   });
 };
 
+exports.import = function(req, res) {
+  User.create(
+    { username: 'bob',
+      password: 'dylan' },
+    { username: 'hank',
+      password: 'williams' },
+    { username: 'john',
+      password: 'prine' },
+    { username: 'townes',
+      password: 'van zandt'}  
+    , function(err) {
+      if (err) {
+        return console.log(err);
+      }
+      return res.send(202);
+    }
+  );
+};
+
+exports.findById = function(req, res) {
+  var id = req.params.id;
+  User.findOne({'_id': id}, function(err, result) {
+    return res.send(result);
+  });
+};
+
+exports.update = function(req, res) {
+  var id = req.params.id;
+  var updates = req.body;
+
+  User.update({'_id': id}, req.body, function (err, numberAffected) {
+    if (err) {
+      return console.log(err);
+    }  
+    console.log('Updated %d users', numberAffected);
+    return res.send(202);
+  });
+};
+
+exports.add = function(req, res) {
+  User.create(req.body, function (err, user) {
+    if (err) {
+      return console.log(err);
+    }
+    return res.send(user);
+  });
+};
+
+exports.delete = function(req, res){
+  var id = req.params.id;
+  User.remove({'_id': id}, function(result) {
+    return res.send(result);
+  });
+};
 
